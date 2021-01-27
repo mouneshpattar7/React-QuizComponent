4221617052028dd8e0c40be39a939995863836be
Mounesh Bhaskar Pattar <M1026883@mindtree.com>
2021-01-27 17:50:37 +0530

    Completed module

---
+++ src/Quiz.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import QuizQuestion from './QuizQuestion'
+
+let quizData = require('./quiz_data.json')
+
+class Quiz extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            quiz_position: 1
+        }
+    }
+
+    showNextQuestion() {
+        this.setState((state) => {
+            return { quiz_position: state.quiz_position + 1 }
+        })
+    }
+
+    handleResetClick(){
+        this.setState({quiz_position: 1 })
+    }
+
+    render() {
+        
+
+        return (
+            <div>
+                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} />
+            </div>
+        )
+    }
+
+}
+
+export default Quiz
---
+++ src/QuizQuestion.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+
+class QuizQuestion extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            incorrectAnswer: false
+        }
+    }
+
+
+    handleClick(buttonText) {
+
+        if (this.props.quiz_question.answer === buttonText) {
+            this.props.showNextQuestionHandler()
+        }
+        this.setState((state) => {
+            return { incorrectAnswer: !state.incorrectAnswer }
+        })
+    }
+
+    render() {
+        return (
+            <main>
+                {this.state.incorrectAnswer ? <p className="error">Sorry, that's not right</p> : null}
+                <section>
+                    <p>{this.props.quiz_question.instruction_text}</p>
+                </section>
+                <section className="buttons">
+                    <ul>
+                      
+                    </ul>
+                </section>
+            </main>
+        )
+    }
+}
+
+export default QuizQuestion
