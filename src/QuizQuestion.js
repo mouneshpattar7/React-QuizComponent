6bdc7bf7fadacb2717eff95ced9f200debc80f68
Mounesh Bhaskar Pattar <M1026883@mindtree.com>
2021-01-27 18:14:10 +0530

    Completed module

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -25,16 +25,13 @@ class QuizQuestion extends Component {
     render() {
         return (
             <main>
-               
+                {this.state.incorrectAnswer ? <p className="error">Sorry, that's not right</p> : null}
                 <section>
                     <p>{this.props.quiz_question.instruction_text}</p>
                 </section>
                 <section className="buttons">
                     <ul>
-                      {this.props.quiz_question.answer_options.map((data, index) => {
-                            return <QuizQuestionButton key={index} button_text={data} clickHandler={this.handleClick.bind(this)} />
-                        }
-                        )}
+                        <li></li>
                     </ul>
                 </section>
             </main>
